8b215ac932db724ffd32fce4d556d3bc213bbf60
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-06-08 16:25:03 -0500

    completed resolvers in schemas and edited typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,50 +1,47 @@
-const { gql } = require('apollo-server-express')
+const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type Book {
-        _id: String
-        authors: [String]
-        description: String
-        bookId: String
-        image: String
-        link: String
-        title: String
-    }
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
 
-    type User {
-        _id: ID
-        username: String
-        email: String
-        password: String
-        savedBooks: [Book]
-    }
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
 
-    type Auth {
-        token: ID!
-        user: User
-    }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-    type Query {
-        users: [User]!
-        user(userId: ID!): User
-    }
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
 
-    type Mutation {
-        addUser(username: String!, email: String!, password: String!): Auth
-        login(email: String!, password: String!): Auth
-        
-        addBook(
-            userId: ID!
-            authors: [String]!
-            description: String!
-            bookId: String!
-            image: String!
-            link: String!
-            title: String!
-        ): User
-        removeUser(userId: ID!): User
-        removeBook(userId: String!, bookId: String!): User
-    }
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
