3f2ae5f94fa52ed25ba7b068619f550d183edc8f
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-06-08 17:01:27 -0500

    trying to debug on client side; changes to index.js, app.js, auth.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
 
 const client = new ApolloClient({
   uri: "/graphql",
