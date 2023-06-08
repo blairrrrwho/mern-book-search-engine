57e1d00e925e09219f1237b4a59db8e36ca1f1cf
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-06-08 16:41:16 -0500

    edited queries in utils on client side

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,19 +1,39 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
-export const QUERY_ME = gql`
-  {
-    me {
-      _id
-      username
-      email
-      savedBooks {
-        bookId
-        authors
-        image
-        description
-        title
-        link
-      }
+export const GET_USERS = gql`
+    query Query {
+        users {
+            _id
+            username
+            email
+            password
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }
     }
-  }
+`;
+
+export const GET_USER = gql`
+    query Query($userId: ID!) {
+        user(userId: $userId) {
+            _id
+            username
+            email
+            password
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }
+    }
 `;
