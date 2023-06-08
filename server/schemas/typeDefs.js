6c21d6e6d62214ccdaecbd1b6bd46030ce2f9697
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-06-08 16:23:35 -0500

    completed typeDefs in schemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,32 +1,50 @@
-const {gql} = require('apollo-server-express')
+const { gql } = require('apollo-server-express')
 
 const typeDefs = gql`
-type User {
+    type Book {
+        _id: String
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
 
-}
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]
+    }
 
-type Book {
+    type Auth {
+        token: ID!
+        user: User
+    }
 
-}
+    type Query {
+        users: [User]!
+        user(userId: ID!): User
+    }
 
-type Auth {
-
-}
-
-input BookInput {
-
-}
-
-type Query {
-    me: 
-}
-
-type Mutation {
-    login():
-    addUser():
-    saveBook():
-    removeBook():
-}
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        
+        addBook(
+            userId: ID!
+            authors: [String]!
+            description: String!
+            bookId: String!
+            image: String!
+            link: String!
+            title: String!
+        ): User
+        removeUser(userId: ID!): User
+        removeBook(userId: String!, bookId: String!): User
+    }
 `;
 
 module.exports = typeDefs;
